a431c21ad81d5c72d1ec8fa9aaacc097919f12da
HusseinAlokla <hussein77.alokla@gmail.com>
2024-01-04 14:08:45 +0200

    hello laravel

---
+++ resources/views/hussein.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Hussein alokla is the best student in the world and he will be a successful software engineeer in the future
+        hopefully </h2>
+    <h6>and he loves fati ofcourse</h6>
+
+</body>
+
+</html>
